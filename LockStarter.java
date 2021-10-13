911a25124387e41cfbbf0fc023bf5659d2e1bb6b
Avila-BS <avila.xworkz@gmail.com>
2021-10-13 23:21:56 +0530

    instance method examples

---
+++ LockStarter.java
@@ -0,0 +1,8 @@
+class LockStarter{
+public static void main(String[] water)
+{
+Lock lock=new Lock();
+lock.open(200);
+lock.close();
+}
+}
---
+++ WatchStarter.java
@@ -0,0 +1,12 @@
+class WatchStarter{
+	public static void main(String[] args)
+	{
+		Watch watch=new Watch();
+		watch.displayDetails();
+		watch.type=WatchType.SOLARPOWERED;
+		watch.countryMade="GERMANY";
+		watch.displayDetails();
+		
+		
+	}
+}
